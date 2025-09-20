084419524a14882d7f80d1ae9892501791ec8ad9
fareez <iqmal3@outlook.com>
2025-09-20 15:27:14 +0800

    :lipstick: refactor Qrcode component to remove deprecated members

--- src/components/QrCodePage/Qrcode.js
+++ src/components/QrCodePage/Qrcode.js
@@ -1,21 +1,20 @@
 import { QRCodeSVG } from 'qrcode.react';
 import queryString from "query-string";
-import {useColorMode} from '@docusaurus/theme-common';
+import { useColorMode } from '@docusaurus/theme-common';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Qrcode() {
-    const { isDarkTheme } = useColorMode();
+    const isDarkTheme = useColorMode().colorMode === 'dark';
 
     return (
         <BrowserOnly fallback={<div>Loading...</div>}>{
             () => {
                 const rawData = queryString.parse(window.location.search).data;
                 return <div style={{ paddingTop: '20px', paddingBottom: '20px' }}>
-                    <QRCodeSVG size={300} value={rawData} includeMargin={isDarkTheme} />
+                    <QRCodeSVG size={300} value={rawData} marginSize={isDarkTheme ? 2 : 0} />
                 </div>
             }
         }
         </BrowserOnly>
-
     );
 };
