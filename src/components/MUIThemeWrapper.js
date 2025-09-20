a777cc517ddf516e3a03bacf4751d9a307b80270
fareez <iqmal3@outlook.com>
2025-09-20 15:20:12 +0800

    :zap: remove deprecated isDarkTheme

--- src/components/MUIThemeWrapper.js
+++ src/components/MUIThemeWrapper.js
@@ -1,21 +1,13 @@
-import React from 'react';
 import { useColorMode } from '@docusaurus/theme-common';
-import {createTheme, ThemeProvider} from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material";
 
 // Make MUI components respects docusaurus's colour mode
-export default function MUIThemeWrapper({children}) {
-  const { isDarkTheme } = useColorMode();
-
-  const darkTheme = createTheme({
-    palette: {
-      mode: isDarkTheme?'dark':'light',
-    }
-  })
+export default function MUIThemeWrapper({ children }) {
+  const darkTheme = createTheme({ palette: { mode: useColorMode().colorMode } });
 
   return (
-        <ThemeProvider theme={darkTheme}>
-        {children}
-        </ThemeProvider>
-
+    <ThemeProvider theme={darkTheme}>
+      {children}
+    </ThemeProvider>
   );
 };
