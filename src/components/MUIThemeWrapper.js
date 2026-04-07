fab204ba641b86d148b5e415ef07f73b4f80c918
fareez <iqmal3@outlook.com>
2026-04-08 07:32:48 +0800

    ♻️ refactor useColorMode hook usage to allow build to pass

--- src/components/MUIThemeWrapper.js
+++ src/components/MUIThemeWrapper.js
@@ -3,7 +3,16 @@ import { createTheme, ThemeProvider } from "@mui/material";
 
 // Make MUI components respects docusaurus's colour mode
 export default function MUIThemeWrapper({ children }) {
-  const darkTheme = createTheme({ palette: { mode: useColorMode().colorMode } });
+  let colorMode = 'light';
+
+  try {
+    colorMode = useColorMode().colorMode;
+  } catch (e) {
+    // When hook failed (likely during SSG). Use default light theme.
+    colorMode = 'light';
+  }
+
+  const darkTheme = createTheme({ palette: { mode: colorMode } });
 
   return (
     <ThemeProvider theme={darkTheme}>
--- src/components/QrCodePage/Qrcode.js
+++ src/components/QrCodePage/Qrcode.js
@@ -3,18 +3,21 @@ import queryString from "query-string";
 import { useColorMode } from '@docusaurus/theme-common';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
-export default function Qrcode() {
+function QrCodeRenderer() {
     const isDarkTheme = useColorMode().colorMode === 'dark';
+    const rawData = queryString.parse(window.location.search).data;
 
     return (
-        <BrowserOnly fallback={<div>Loading...</div>}>{
-            () => {
-                const rawData = queryString.parse(window.location.search).data;
-                return <div style={{ paddingTop: '20px', paddingBottom: '20px' }}>
-                    <QRCodeSVG size={300} value={rawData} marginSize={isDarkTheme ? 2 : 0} />
-                </div>
-            }
-        }
+        <div style={{ paddingTop: '20px', paddingBottom: '20px' }}>
+            <QRCodeSVG size={300} value={rawData} marginSize={isDarkTheme ? 2 : 0} />
+        </div>
+    );
+}
+
+export default function Qrcode() {
+    return (
+        <BrowserOnly fallback={<div>Loading...</div>}>
+            {() => <QrCodeRenderer />}
         </BrowserOnly>
     );
 };
